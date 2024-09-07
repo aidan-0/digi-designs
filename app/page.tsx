0d1291d797382783c932fef286c2e926710ed16d
Aidan <am.mcdonald00@gmail.com>
2024-09-07 12:35:43 +0800

    Add Digi Designs logo and fonts

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Hello</div>
+    <div className="text-3xl font-semibold text-digi-med-blue">Our mission is simple; make you a damn good website.</div>
   );
 }
