4a4182640f8387210cb1aa0c65b08e07624cc65f
Aidan <am.mcdonald00@gmail.com>
2024-10-02 19:53:47 +0800

    clear form on submission

--- components/ContactForm.tsx
+++ components/ContactForm.tsx
@@ -1,13 +1,35 @@
 'use client'
 
-import { useState } from 'react'
+import { useState, ChangeEvent } from 'react'
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { toast } from "@/hooks/use-toast"
 
+interface FormData {
+  name: string;
+  email: string;
+  phone: string;
+  message: string;
+}
+
 export default function ContactForm() {
   const [isSubmitting, setIsSubmitting] = useState(false)
+  const [formData, setFormData] = useState<FormData>({
+    name: '',
+    email: '',
+    phone: '',
+    message: '',
+  })
+
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = event.target
+    console.log(formData)
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }))
+  }
 
   async function onSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault()
@@ -32,8 +54,12 @@ export default function ContactForm() {
         title: "Success",
         description: "Your message has been sent successfully!",
       })
-      event.currentTarget.reset()
-
+      setFormData({
+        name: '',
+        email: '',
+        phone: '',
+        message: '',
+      })
     } else {
       toast({
         title: "Error",
@@ -49,18 +75,50 @@ export default function ContactForm() {
         <h4 className='hidden md:flex text-2xl'>Contact Us</h4>
       </div>
       <div className="space-y-2">
-        <Input id="name" name="name" placeholder='Name' required />
+        <Input 
+          id="name" 
+          name="name" 
+          placeholder='Name' 
+          required 
+          value={formData.name} 
+          onChange={handleInputChange} 
+        />
       </div>
       <div className="space-y-2">
-        <Input id="email" name="email" type="email" placeholder='Email' required />
+        <Input 
+          id="email" 
+          name="email" 
+          type="email" 
+          placeholder='Email' 
+          required 
+          value={formData.email} 
+          onChange={handleInputChange} 
+        />
       </div>
       <div className="space-y-2">
-        <Input id="phone" name="phone" type="tel" placeholder='Phone' />
+        <Input 
+          id="phone" 
+          name="phone" 
+          type="tel" 
+          placeholder='Phone' 
+          value={formData.phone} 
+          onChange={handleInputChange} 
+        />
       </div>
       <div className="space-y-2">
-        <Textarea id="message" name="message" placeholder='Message or Request' required />
+        <Textarea 
+          id="message" 
+          name="message" 
+          placeholder='Message or Request' 
+          required 
+          value={formData.message} 
+          onChange={handleInputChange} 
+        />
       </div>
-      <Button type="submit" disabled={isSubmitting} className="w-full md:w-auto cursor-pointer px-4 sm:px-5 py-5 font-semibold hover:bg-dd-light hover:text-dd-dark rounded-[6px] text-dd-light border-dd-dark dark:border-card border-2  bg-dd-dark dark:bg-foreground dark:text-dd-dark transition duration-200 text-lg hover:shadow-[1px_1px_rgba(78,158,221),2px_2px_rgba(78,158,221),3px_3px_rgba(78,158,221),4px_4px_rgba(78,158,221)] dark:hover:shadow-[1px_1px_rgba(78,158,221),2px_2px_rgba(78,158,221),3px_3px_rgba(78,158,221),4px_4px_rgba(78,158,221)]"
+      <Button 
+        type="submit" 
+        disabled={isSubmitting} 
+        className="w-full md:w-auto cursor-pointer px-4 sm:px-5 py-5 font-semibold hover:bg-dd-light hover:text-dd-dark rounded-[6px] text-dd-light border-dd-dark dark:border-card border-2  bg-dd-dark dark:bg-foreground dark:text-dd-dark transition duration-200 text-lg hover:shadow-[1px_1px_rgba(78,158,221),2px_2px_rgba(78,158,221),3px_3px_rgba(78,158,221),4px_4px_rgba(78,158,221)] dark:hover:shadow-[1px_1px_rgba(78,158,221),2px_2px_rgba(78,158,221),3px_3px_rgba(78,158,221),4px_4px_rgba(78,158,221)]"
       >
         {isSubmitting ? 'Sending...' : 'Send Message'}
       </Button>
